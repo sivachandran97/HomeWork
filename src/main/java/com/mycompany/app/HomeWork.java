728c01604b952ca8f26d87a67fdd7d03ea509014
Welcome <Welcome@SivaChandran>
2025-06-15 19:53:27 +0530

    Code commit

--- src/main/java/com/mycompany/app/HomeWork.java
+++ src/main/java/com/mycompany/app/HomeWork.java
@@ -1,13 +1,14 @@
 package com.mycompany.app;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
-public class App {
+public class HomeWork {
     public static void main(String[] args) {
-        
+        SpringApplication.run(HomeWork.class, args);
     }
 }
