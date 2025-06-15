a27cac415ff4bfe12a5ab30b1448ac3600d26e52
Welcome <Welcome@SivaChandran>
2025-06-15 19:31:28 +0530

    Initial project setup

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
